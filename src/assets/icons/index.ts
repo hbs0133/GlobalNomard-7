feeffb31e75605f82433659c6c0d3ee2a19e885a
hbs0133 <hbs0133@naver.com>
2024-09-04 10:22:34 +0900

    Chore: 아이콘,이미지 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,75 @@
+import IconAccountCheckOutline from './ic_account-check-outline.svg';
+import IconArrowCircle from './ic_arrow_circle.svg';
+import IconArrowLeftBlack32px from './ic_arrow_left_black_32px.svg';
+import IconArrowRightBlack32px from './ic_arrow_right_black_32px.svg';
+import IconArrowLeftGray32px from './ic_arrow_left_gray_32px.svg';
+import IconArrowRightGray32px from './ic_arrow_right_gray_32px.svg';
+import IconArrowLeftBlack48px from './ic_arrow_left_black_48px.svg';
+import IconArrowRightBlack48px from './ic_arrow_right_black_48px.svg';
+import IconArrowLeftGray48px from './ic_arrow_left_gray_48px.svg';
+import IconArrowRightGray48px from './ic_arrow_right_gray_48px.svg';
+import IconBed from './ic_bed.svg';
+import IconCalendar from './ic_calendar.svg';
+import IconCalendarCheck from './ic_calendar_check_outline.svg';
+import IconCog from './ic_cog.svg';
+import IconDelete24px from './ic_delete_24px.svg';
+import IconDelete32px from './ic_delete_32px.svg';
+import IconDelete40px from './ic_delete_40px.svg';
+import IconFacebook from './ic_facebook-fill.svg';
+import IconInstargram from './ic_instagram-fill.svg';
+import IconTwiter from './ic_twitter-fill.svg';
+import IconYoutube from './ic_youtube-fill.svg';
+import IconImageRegister from './ic_image_registration.svg';
+import IconMeatball from './ic_meatball.svg';
+import IconMinusTime from './ic_minus_time.svg';
+import IconPlusTime from './ic_plus_time.svg';
+import IconNotification from './ic_notification.svg';
+import IconArrowLeftPagination from './ic_pagination_arrow_left.svg';
+import IconArrowRightPagination from './ic_pagination_arrow_right.svg';
+import IconStarOff from './ic_star_off.svg';
+import IconStarOn from './ic_star_on.svg';
+import IconTextBoxCheck from './ic_text-box-check-outline.svg';
+import IconVisibilityOff from './ic_visibility_off.svg';
+import IconVisibilityOn from './ic_visibility_on.svg';
+import IconX40px from './ic_x_40px.svg';
+import IconXBold from './ic_x_bold_24px.svg';
+import IconXMedium from './ic_x_medium_24px.svg';
+
+export {
+  IconAccountCheckOutline,
+  IconArrowCircle,
+  IconArrowLeftBlack32px,
+  IconArrowRightBlack32px,
+  IconArrowLeftGray32px,
+  IconArrowRightGray32px,
+  IconArrowLeftBlack48px,
+  IconArrowRightBlack48px,
+  IconArrowLeftGray48px,
+  IconArrowRightGray48px,
+  IconBed,
+  IconCalendar,
+  IconCalendarCheck,
+  IconCog,
+  IconDelete24px,
+  IconDelete32px,
+  IconDelete40px,
+  IconFacebook,
+  IconInstargram,
+  IconTwiter,
+  IconYoutube,
+  IconImageRegister,
+  IconMeatball,
+  IconMinusTime,
+  IconPlusTime,
+  IconNotification,
+  IconArrowLeftPagination,
+  IconArrowRightPagination,
+  IconStarOff,
+  IconStarOn,
+  IconTextBoxCheck,
+  IconVisibilityOff,
+  IconVisibilityOn,
+  IconX40px,
+  IconXBold,
+  IconXMedium,
+};
