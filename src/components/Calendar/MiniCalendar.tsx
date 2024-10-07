a421a38317487be81a2e3bff08c99e01bedd5989
yeram7591 <dhflgjsxj2@naver.com>
2024-10-07 13:57:52 +0900

    Feat: 모달 및 예약현황 페이지 기능 구현 중

--- src/components/Calendar/MiniCalendar.tsx
+++ src/components/Calendar/MiniCalendar.tsx
@@ -1,9 +1,62 @@
 import React, { useState } from 'react';
 import { IconCalendarNext, IconCalendarPrev } from '@/assets/icons';
 import Image from 'next/image';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
-const MiniCalendar: React.FC = () => {
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+const fetchAvailableSchedules = async (activityId, year, month) => {
+  const response = await apiClient.get(
+    `/activities/${activityId}/available-schedule`,
+    {
+      params: { year, month },
+    },
+  );
+  return response.data;
+};
+
+function MiniCalendar({ activityId }) {
   const [currentDate, setCurrentDate] = useState(new Date());
+  const [selectedDay, setSelectedDay] = useState<number | null>(null);
+
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+
+  const {
+    data: availableSchedules,
+    isLoading,
+    error,
+  } = useQuery(
+    ['availableSchedules', activityId, currentYear, currentMonth, selectedDay],
+    () => {
+      if (selectedDay) {
+        const year = currentYear.toString();
+        return fetchAvailableSchedules(activityId, year, currentMonth);
+      }
+      return null;
+    },
+    {
+      enabled: !!selectedDay,
+    },
+  );
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
   const daysInMonth = (date: Date) => {
     const year = date.getFullYear();
@@ -13,6 +66,10 @@ const MiniCalendar: React.FC = () => {
 
   const firstDayOfMonth = (date: Date) => {
     return new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+  };
+
+  const handleDayClick = (day: number) => {
+    setSelectedDay(day);
   };
 
   const renderDays = () => {
@@ -47,6 +104,7 @@ const MiniCalendar: React.FC = () => {
       days.push(
         <div
           key={i}
+          onClick={() => handleDayClick(i)}
           className={`flex h-[32px] w-full items-center justify-center p-[10px] text-sm font-semibold ${isToday ? 'rounded-[8px] bg-green-ce' : 'text-gray-4b'}`}
         >
           {i}
@@ -59,7 +117,7 @@ const MiniCalendar: React.FC = () => {
       days.push(
         <div
           key={`next-${i}`}
-          className="text-gray-aaa flex h-[32px] w-full items-center justify-center p-[10px] text-sm font-semibold text-gray-a4"
+          className="flex h-[32px] w-full items-center justify-center p-[10px] text-sm font-semibold text-gray-a4"
         >
           {i}
         </div>,
@@ -86,7 +144,7 @@ const MiniCalendar: React.FC = () => {
             alt="이전 달력 버튼"
           />
         </button>
-        <h2 className="text-md font-bold">{`${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`}</h2>
+        <h2 className="text-md font-bold">{` ${monthNames[currentDate.getMonth()]} ${currentDate.getFullYear()}`}</h2>
         <button type="button" onClick={() => changeMonth(1)}>
           <Image
             className="h-[16px] w-[16px]"
@@ -108,6 +166,6 @@ const MiniCalendar: React.FC = () => {
       </div>
     </div>
   );
-};
+}
 
 export default MiniCalendar;
