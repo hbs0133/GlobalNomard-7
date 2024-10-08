7207ece621c603c93b9cf2c5c89b729523312fcf
hbs0133 <hbs0133@naver.com>
2024-10-08 13:08:36 +0900

    Fix: 유효성검사 수정

--- src/containers/register/Scheduleinput.tsx
+++ src/containers/register/Scheduleinput.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import Image from 'next/image';
-import { IconPlusTime, IconMinusTime } from '@/assets/icons';
+import { IconPlusTime } from '@/assets/icons';
 
 interface ScheduleInputProps {
-  time: { date: Date; startTime: string; endTime: string };
-  onScheduleChange: (date: Date, startTime: string, endTime: string) => void;
+  time: { date: Date | null; startTime: string; endTime: string };
+  onScheduleChange: (
+    date: Date | null,
+    startTime: string,
+    endTime: string,
+  ) => void;
   addSchedule: () => void;
 }
 
@@ -16,36 +20,42 @@ const ScheduleInput: React.FC<ScheduleInputProps> = ({
   addSchedule,
 }) => {
   return (
-    <div className="flex justify-between">
-      <DatePicker
-        selected={time.date}
-        onChange={(date) =>
-          onScheduleChange(date as Date, time.startTime, time.endTime)
-        }
-        dateFormat="yyyy-MM-dd"
-        className="block h-[56px] w-[379px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
-      />
-      <input
-        type="time"
-        value={time.startTime}
-        onChange={(e) =>
-          onScheduleChange(time.date, e.target.value, time.endTime)
-        }
-        className="block h-[56px] w-[140px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
-      />
-      <input
-        type="time"
-        value={time.endTime}
-        onChange={(e) =>
-          onScheduleChange(time.date, time.startTime, e.target.value)
-        }
-        className="block h-[56px] w-[140px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
-      />
-      <button
-        type="button"
-        onClick={addSchedule}
-        className="text-blue-500 hover:text-blue-700"
-      >
+    <div className="flex flex-row items-center justify-between">
+      <div className="flex flex-col gap-[10px]">
+        <label>날짜</label>
+        <DatePicker
+          selected={time.date} // 수정
+          onChange={(date) => {
+            onScheduleChange(date, time.startTime, time.endTime);
+          }}
+          dateFormat="yyyy-MM-dd"
+          className="block h-[56px] w-[379px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
+          placeholderText="YY/MM/DD"
+        />
+      </div>
+      <div className="flex flex-col gap-[10px]">
+        <label>시작시간</label>
+        <input
+          type="time"
+          value={time.startTime}
+          onChange={(e) =>
+            onScheduleChange(time.date, e.target.value, time.endTime)
+          }
+          className="block h-[56px] w-[140px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
+        />
+      </div>
+      <div className="flex flex-col gap-[10px]">
+        <label>종료시간</label>
+        <input
+          type="time"
+          value={time.endTime}
+          onChange={(e) =>
+            onScheduleChange(time.date, time.startTime, e.target.value)
+          }
+          className="block h-[56px] w-[140px] rounded-[4px] border border-gray-79 pb-[8px] pl-[16px] pt-[8px]"
+        />
+      </div>
+      <button type="button" onClick={addSchedule} className="mt-auto">
         <Image
           src={IconPlusTime}
           alt="일정을 추가하는 +모양 아이콘"
